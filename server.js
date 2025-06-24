8855c85546c23c5539b22c35e8b44c99d3c50e2e
reshma <reshmamutyala77@gmail.com>
2025-06-24 15:19:53 +0530

    smart booking

--- server.js
+++ server.js
@@ -156,10 +156,8 @@ app.listen(PORT, () => {
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('✅ Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
 let isAdminLoggedIn = false;
